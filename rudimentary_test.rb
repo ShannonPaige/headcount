05b1c3ed7072c971458ef198785dfde7db5e0d51
ShannonPaige <spaige@gmail.com>
2015-09-15 14:27:51 -0600

    wrote method for parsing csv

--- rudimentary_test.rb
+++ rudimentary_test.rb
@@ -1,19 +1,28 @@
 
-handle = File.open("fixture_3rd_grade.csv")  # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - fixture_3rd_grade
-
-location = []
-number = []
+handle = File.open("./fixture_3rd_grade.csv")   # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - fixture_3rd_grade.csv
+headers = []
+has_headers = true
+third_grade = []
 handle.each_line do |line|
+  hash = {}
   parsed_data = line.split(",")
-  location << parsed_data[0]
-  number << parsed_data[1].chomp
+  parsed_data[parsed_data.length - 1].chomp!
+  if has_headers
+    headers = parsed_data
+    has_headers = false
+  else
+    for x in 0...parsed_data.length
+      hash[headers[x]] = parsed_data[x]
+    end
+    third_grade << hash
+  end
 end
-puts "#{location} \n #{number}"
-number
+
+p third_grade
 
 # ~> Errno::ENOENT
-# ~> No such file or directory @ rb_sysopen - fixture_3rd_grade
+# ~> No such file or directory @ rb_sysopen - fixture_3rd_grade.csv
 # ~>
-# ~> /Users/charissalawrence/Documents/Turing/Module1/headcount/test.rb:2:in `initialize'
-# ~> /Users/charissalawrence/Documents/Turing/Module1/headcount/test.rb:2:in `open'
-# ~> /Users/charissalawrence/Documents/Turing/Module1/headcount/test.rb:2:in `<main>'
+# ~> /Users/shannonpaige/code/headcount/rudimentary_test.rb:2:in `initialize'
+# ~> /Users/shannonpaige/code/headcount/rudimentary_test.rb:2:in `open'
+# ~> /Users/shannonpaige/code/headcount/rudimentary_test.rb:2:in `<main>'
