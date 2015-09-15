fc037dc9301ce9ec7d1f3789ae35f3d00dc272bf
ZooBot Mars <sasa345@yahoo.com>
2015-09-15 13:16:40 -0600

    setting up economic profile

---
+++ lib/economic_profile.rb
@@ -0,0 +1,23 @@
+class EconomicProfile
+
+  def median_household_income
+
+  end
+
+  def school_aged_children_in_poverty
+
+  end
+
+  def free_or_reduced_lunch
+
+  end
+
+  def free_or_reduced_lunch_in_year(year)
+
+  end
+
+  def title_I_students
+
+  end
+
+end
---
+++ test/economic_profile_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "district_repository"
+
+class DistrictTest < Minitest::Test
+
+  def test_median_household_income
+    skip
+  end
+
+  def test_school_aged_children_in_poverty
+    skip
+  end
+
+  def test_free_or_reduced_lunch
+    skip
+  end
+
+  def test_free_or_reduced_lunch_in_year(year)
+    skip
+  end
+
+  def test_title_I_students
+    skip
+  end
+
+end
