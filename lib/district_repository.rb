c2685d9e6ced786b498dc77cd99225ceaca6c2ce
ShannonPaige <spaige@gmail.com>
2015-09-17 11:23:49 -0600

    Passing

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,35 +1,32 @@
-require "csv"                # => true
-require_relative "district"  # => true
+require "csv"
+require_relative "district"
 # require "pry"
 
 class DistrictRepository
-  attr_accessor :data     # => nil
+  attr_accessor :data
 
   def initialize(data)
     @data = data
-  end                   # => :initialize
+  end
 
   def self.load_from_csv(data_dir)
-    data = CSV.read(data_dir, headers: true, header_converters: :symbol).map { |row| row.to_h }  # ~> Errno::ENOENT: No such file or directory @ rb_sysopen - ../data/Pupil enrollment.csv
+    data = CSV.read(File.join(data_dir, 'Pupil enrollment.csv'), headers: true, header_converters: :symbol).map { |row| row.to_h }
+
     grouped = data.group_by do |hash|
       hash.fetch(:location)
     end
 
-      data_array = grouped.map do |key, hashes|
-        enrollment_data = hashes.map do |hash|
-            mapped_data = hashes.map do |hash|
-              [hash[:timeframe], hash[:data]]
-            end
-            [:enrollment, mapped_data.to_h] if data_dir == './data/Pupil enrollment.csv'
-            [:pupil_enrollment, mapped_data.to_h] if data_dir == './data/Online pupil enrollment.csv'
-        end
-        [key, enrollment_data.to_h]
+    data_array = grouped.map do |key, hashes|
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe], hash[:data]]
       end
-      data_hash = data_array.to_h
+      [key, {enrollment: mapped_data.to_h}]
     end
-    if data_dir == './data/'
+
+    data_hash = data_array.to_h
     DistrictRepository.new(data_hash)
-  end                                                                                            # => :load_from_csv
+  end
+
 
   def find_by_name(district_name)
     district_exists = data.any? do |hash|
@@ -38,25 +35,15 @@ class DistrictRepository
       end
     end
     District.new(district_name, data.fetch(district_name)) if district_exists
-  end                                                     # => :find_by_name
+  end
 
   def find_all_matching
 
-  end  # => :find_all_matching
+  end
 
-end  # => :find_all_matching
+end
 
-if $PROGRAM_NAME == __FILE__                                             # => true
+if $PROGRAM_NAME == __FILE__
   dr = DistrictRepository.load_from_csv('../data/Pupil enrollment.csv')
   district = dr.find_by_name("ACADEMY 20")
 end
-
-# ~> Errno::ENOENT
-# ~> No such file or directory @ rb_sysopen - ../data/Pupil enrollment.csv
-# ~>
-# ~> /Users/shannonpaige/.rvm/rubies/ruby-2.2.2/lib/ruby/2.2.0/csv.rb:1256:in `initialize'
-# ~> /Users/shannonpaige/.rvm/rubies/ruby-2.2.2/lib/ruby/2.2.0/csv.rb:1256:in `open'
-# ~> /Users/shannonpaige/.rvm/rubies/ruby-2.2.2/lib/ruby/2.2.0/csv.rb:1256:in `open'
-# ~> /Users/shannonpaige/.rvm/rubies/ruby-2.2.2/lib/ruby/2.2.0/csv.rb:1330:in `read'
-# ~> /Users/shannonpaige/code/headcount/lib/district_repository.rb:13:in `load_from_csv'
-# ~> /Users/shannonpaige/code/headcount/lib/district_repository.rb:42:in `<main>'
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -5,18 +5,19 @@ require "district_repository"
 
 class DistrictRepositoryTest < Minitest::Test
 
-  # def data_dir
-  #   File.expand_path '../data', __dir__
-  # end
+  def data_dir
+    File.expand_path '../data', __dir__
+  end
   meta(current: true)
   def test_it_can_load_a_district_from_csv_data
-    dr = DistrictRepository.load_from_csv('./data/Pupil enrollment.csv')
+    dr = DistrictRepository.load_from_csv(data_dir)
     district = dr.find_by_name("ACADEMY 20")
     assert_equal "ACADEMY 20", dr.find_by_name("ACADEMY 20").district_name
     assert_equal "22620", dr.find_by_name("ACADEMY 20").enrollment.in_year(2009)
     # assert_equal 0.895, district.enrollment.graduation_rate.for_high_school_in_year(2010)
     # assert_equal 0.857, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2008)
   end
+
 
   def test_returns_nil_if_district_doesnt_exist
     skip
