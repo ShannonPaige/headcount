ec68192f467198815bb0da05f35cf1bc38de4fbf
ShannonPaige <spaige@gmail.com>
2015-09-17 16:23:01 -0600

    added data type one parsing method

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -9,34 +9,32 @@ class DistrictRepository
     @data = data
   end
 
+  def self.load_data_type_one(data_dir, data_hash, file)
+    filename = file.gsub('.csv', "").gsub(" ", '_').downcase.to_sym
+    data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
+    grouped = data.group_by do |hash|
+      hash.fetch(:location)
+    end
+    grouped.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_i]
+      end
+      data_hash[district_name][filename] = mapped_data.to_h
+    end
+  end
+
   def self.load_from_csv(data_dir)
     data_hash = {}
 
     # reads in the Pupil enrollment file
-    data = CSV.read(File.join(data_dir, 'Pupil enrollment.csv'), headers: true, header_converters: :symbol).map { |row| row.to_h }
-    grouped = data.group_by do |hash|
-      hash.fetch(:location)
-    end
-    grouped.each do |district_name, hashes|
-      data_hash[district_name] ||= {}
-      mapped_data = hashes.map do |hash|
-        [hash[:timeframe].to_i, hash[:data].to_i]
-      end
-      data_hash[district_name][:pupil_enrollment] = mapped_data.to_h
-    end
-
-    #reads in the online pupil enrollment file
-    data = CSV.read(File.join(data_dir, 'Online pupil enrollment.csv'), headers: true, header_converters: :symbol).map { |row| row.to_h }
-    grouped = data.group_by do |hash|
-      hash.fetch(:location)
-    end
-    grouped.each do |district_name, hashes|
-      data_hash[district_name] ||= {}
-      mapped_data = hashes.map do |hash|
-        [hash[:timeframe].to_i, hash[:data].to_i]
-      end
-      data_hash[district_name][:online_pupil_enrollment] = mapped_data.to_h
-    end
+    load_data_type_one(data_dir, data_hash, 'Pupil enrollment.csv')
+    load_data_type_one(data_dir, data_hash, 'Online pupil enrollment.csv')
+    load_data_type_one(data_dir, data_hash, 'High school graduation rates.csv')
+    load_data_type_one(data_dir, data_hash, 'Kindergartners in full-day program.csv')
+    load_data_type_one(data_dir, data_hash, 'Median household income.csv')
+    load_data_type_one(data_dir, data_hash, 'Remediation in higher education.csv')
+    load_data_type_one(data_dir, data_hash, 'Title I students.csv')
 
     #reads in online pupil enrollment by race
     data = CSV.read(File.join(data_dir, 'Pupil enrollment by race_ethnicity.csv'), headers: true, header_converters: :symbol).map { |row| row.to_h }
