09c982b7291539e38aca66c4e7b161608655df8b
ShannonPaige <spaige@gmail.com>
2015-09-17 16:44:07 -0600

    added data type two parsing method

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -9,7 +9,7 @@ class DistrictRepository
     @data = data
   end
 
-  def self.load_data_type_one(data_dir, data_hash, file)
+  def self.parse_data_type_1(data_dir, data_hash, file)
     filename = file.gsub('.csv', "").gsub(" ", '_').downcase.to_sym
     data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
     grouped = data.group_by do |hash|
@@ -24,35 +24,44 @@ class DistrictRepository
     end
   end
 
-  def self.load_from_csv(data_dir)
-    data_hash = {}
-
-    # reads in the Pupil enrollment file
-    load_data_type_one(data_dir, data_hash, 'Pupil enrollment.csv')
-    load_data_type_one(data_dir, data_hash, 'Online pupil enrollment.csv')
-    load_data_type_one(data_dir, data_hash, 'High school graduation rates.csv')
-    load_data_type_one(data_dir, data_hash, 'Kindergartners in full-day program.csv')
-    load_data_type_one(data_dir, data_hash, 'Median household income.csv')
-    load_data_type_one(data_dir, data_hash, 'Remediation in higher education.csv')
-    load_data_type_one(data_dir, data_hash, 'Title I students.csv')
-
-    #reads in online pupil enrollment by race
-    data = CSV.read(File.join(data_dir, 'Pupil enrollment by race_ethnicity.csv'), headers: true, header_converters: :symbol).map { |row| row.to_h }
+  def self.parse_data_type_2(data_dir, data_hash, file)
+    filename = file.gsub('.csv', "").gsub(" ", '_').downcase.to_sym
+    data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
+    info = ""
+    data.each do |hash|
+      info = hash.keys[1]
+    end
     grouped = data.group_by do |hash|
       hash.fetch(:location)
     end
     grouped.each do |district_name, hashes|
       data_hash[district_name] ||= {}
       mapped_data = hashes.map do |hash|
-        [hash[:timeframe], hash[:data]]
+        [hash[:timeframe].to_i, hash[:data].to_i]
       end
       mapped_data = mapped_data.to_h
-      raced_map = hashes.map do |hash|
-        [hash[:race], mapped_data]
+      info_map = hashes.map do |hash|
+        [hash[info], mapped_data]
       end
-      raced_map.to_h
-    data_hash[district_name][:pupil_enrollment_by_race] = raced_map.to_h
+    data_hash[district_name][filename] = info_map.to_h
     end
+  end
+
+  def self.load_from_csv(data_dir)
+    data_hash = {}
+    parse_data_type_1(data_dir, data_hash, 'Pupil enrollment.csv')
+    parse_data_type_1(data_dir, data_hash, 'Online pupil enrollment.csv')
+    parse_data_type_1(data_dir, data_hash, 'High school graduation rates.csv')
+    parse_data_type_1(data_dir, data_hash, 'Kindergartners in full-day program.csv')
+    parse_data_type_1(data_dir, data_hash, 'Median household income.csv')
+    parse_data_type_1(data_dir, data_hash, 'Remediation in higher education.csv')
+    parse_data_type_1(data_dir, data_hash, 'Title I students.csv')
+    parse_data_type_2(data_dir, data_hash, '3rd grade students scoring proficient or above on the CSAP_TCAP.csv')
+    parse_data_type_2(data_dir, data_hash, '8th grade students scoring proficient or above on the CSAP_TCAP.csv')
+    parse_data_type_2(data_dir, data_hash, 'Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv')
+    parse_data_type_2(data_dir, data_hash, 'Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv')
+    parse_data_type_2(data_dir, data_hash, 'Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv')
+    parse_data_type_2(data_dir, data_hash, 'Dropout rates by race and ethnicity.csv')
 
     DistrictRepository.new(data_hash)
   end
