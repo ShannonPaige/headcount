2c36f2093564b41ef758e1d0da7be11c535fbee0
ZooBot Mars <sasa345@yahoo.com>
2015-09-18 02:05:31 -0600

    completed parsing methods 3, 4, and 5

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -47,6 +47,126 @@ class DistrictRepository
     end
   end
 
+  def self.parse_data_type_3(data_dir, data_hash, file)
+    data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
+
+    #create hash with whole number values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    numbers = data.select { |hash| hash[:dataformat].include?("Number")}
+    filename << "_by_number"
+    filename = filename.to_sym
+    numbers = numbers.group_by do |hash|
+        hash.fetch(:location)
+    end
+
+    numbers.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_i]
+      end
+      data_hash[district_name][filename] = mapped_data.to_h
+    end
+
+    #create hash with percent values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    percent = data.select { |hash| hash[:dataformat].include?("Percent")}
+    filename << "_by_percentage"
+    filename = filename.to_sym
+    percent = percent.group_by do |hash|
+        hash.fetch(:location)
+    end
+
+    percent.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_f]
+      end
+      data_hash[district_name][filename] = mapped_data.to_h
+    end
+    data_hash
+  end
+
+  def self.parse_data_type_4(data_dir, data_hash, file)
+    data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
+    #create hash with whole number values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    numbers = data.select { |hash| hash[:dataformat].include?("Number")}
+    filename << "_by_number"
+    filename = filename.to_sym
+    numbers = numbers.group_by do |hash|
+        hash.fetch(:race)
+    end
+
+    numbers.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_i]
+      end
+      data_hash[district_name][filename] = mapped_data.to_h
+    end
+
+    #create hash with percent values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    percent = data.select { |hash| hash[:dataformat].include?("Percent")}
+    filename << "_by_percentage"
+    filename = filename.to_sym
+    percent = percent.group_by do |hash|
+        hash.fetch(:race)
+    end
+
+    percent.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_f]
+      end
+      data_hash[district_name][filename] = mapped_data.to_h
+    end
+  end
+
+  def self.parse_data_type_5(data_dir, data_hash, file)
+    data = CSV.read(File.join(data_dir, file), headers: true, header_converters: :symbol).map { |row| row.to_h }
+    #create hash with whole number values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    group_by_numbers = data.select { |hash| hash[:dataformat].include?("Number")}
+    filename << "_by_number"
+    filename = filename.to_sym
+    group_by_numbers = group_by_numbers.group_by do |hash|
+        hash.fetch(:location)
+    end
+    group_by_numbers.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        [hash[:timeframe].to_i, hash[:data].to_i]
+      end
+      mapped_data = mapped_data.to_h
+      info_map = hashes.map do |hash|
+        [hash[:poverty_level], mapped_data]
+      end
+    data_hash[district_name][filename] = info_map.to_h
+    end
+
+    #create hash with percentage values
+    filename = file.gsub('.csv', "").gsub('3', "three").gsub('8', "eight").gsub(" ", '_').gsub("-", '_').downcase
+    group_by_percent = data.select { |hash| hash[:dataformat].include?("Percent")}
+    filename << "_by_percentage"
+    filename = filename.to_sym
+    group_by_percent = group_by_percent.group_by do |hash|
+        hash.fetch(:location)
+    end
+    group_by_percent.each do |district_name, hashes|
+      data_hash[district_name] ||= {}
+      mapped_data = hashes.map do |hash|
+        hash[:data].to_f
+        [hash[:timeframe].to_i, hash[:data].to_f]
+      end
+      mapped_data = mapped_data.to_h
+      info_map = hashes.map do |hash|
+        [hash[:poverty_level], mapped_data]
+      end
+    data_hash[district_name][filename] = info_map.to_h
+    end
+  end
+
   def self.load_from_csv(data_dir)
     data_hash = {}
     parse_data_type_1(data_dir, data_hash, 'Pupil enrollment.csv')
@@ -62,14 +182,16 @@ class DistrictRepository
     parse_data_type_2(data_dir, data_hash, 'Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv')
     parse_data_type_2(data_dir, data_hash, 'Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv')
     parse_data_type_2(data_dir, data_hash, 'Dropout rates by race and ethnicity.csv')
+    parse_data_type_3(data_dir, data_hash, 'Special education.csv')
+    parse_data_type_3(data_dir, data_hash, 'School-aged children in poverty.csv')
+    parse_data_type_4(data_dir, data_hash, 'Pupil enrollment by race_ethnicity.csv')
+    parse_data_type_5(data_dir, data_hash, 'Students qualifying for free or reduced price lunch.csv')
 
     DistrictRepository.new(data_hash)
   end
 
-
-
   def find_by_name(district_name)
-    district_names = data.keys # => ["Colorado", "ACADEMY 20", "ADAMS COUNTY 14", "ADAMS-ARAPAHOE 28J", "AGATE 300", "AGUILAR REORGANIZED 6", "AKRON R-1", "ALAMOSA RE-11J", "ARCHULETA COUNTY 50 JT", "ARICKAREE R-2", "ARRIBA-FLAGLER C-20", "ASPEN 1", "AULT-HIGHLAND RE-9", "BAYFIELD 10 JT-R", "BENNETT 29J", "BETHUNE R-5", "BIG SANDY 100J", "BOULDER VALLEY RE 2", "BRANSON REORGANIZED 82", "BRIGGSDALE RE-10", "BRIGHTON 27J", "BRUSH RE-2(J)", "BUENA VISTA R-31", "BUFFALO RE-4", "BURLINGTON RE-6J", "BYERS 32J", "CALHAN RJ-1", "CAMPO RE-6", "CANON CITY RE-1", "CENTENNIAL R-1", "CENTER 26 JT", "CHERAW 31", "CHERRY CREEK 5", "CHEYENNE COUNTY RE-5", "CHEYENNE MOUNTAIN 12", "CLEAR CREEK RE-1", "COLORADO SPRINGS 11", "COTOPAXI RE-3", "CREEDE CONSOLIDATED 1", "CRIPPLE CREEK-VICTOR RE-1", "CROWLEY COUNTY RE-1-J", "CUSTER COUNTY SCHOOL DISTRICT C-1", "DE BEQUE 49JT", "DEER TRAIL 26J", "DEL NORTE C-7", "DELTA COUNTY 50(J)", "DENVER COUNTY 1", "DOLORES COUNTY RE NO.2", "DOLORES RE-4A", "DOUGLAS COUN...
+    district_names = data.keys
     district_exists = district_names.any? do |name|
       name == district_name
     end
@@ -85,11 +207,5 @@ end
 if $PROGRAM_NAME == __FILE__
   data_dir = File.expand_path '../data', __dir__
   dr = DistrictRepository.load_from_csv(data_dir)
-  district = dr.find_by_name("Shannon").district_name # ~> NoMethodError: undefined method `district_name' for nil:NilClass
-  # =>
+  district = dr.find_by_name("Shannon").district_name
 end
-
-# ~> NoMethodError
-# ~> undefined method `district_name' for nil:NilClass
-# ~>
-# ~> /Users/shannonpaige/code/headcount/lib/district_repository.rb:89:in `<main>'
